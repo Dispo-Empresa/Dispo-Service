0cc9d984a3fd1ba432190a284d7a17b1434489ed
Arthur Molinari <arthureduardomolinari@gmail.com>
2024-02-19 21:19:10 -0300

    104 - Remoçao de regra incorreta para tipos numéricos.

--- src/Modules/Shared/Dispo.Shared.Filter/Services/FilterService.cs
+++ src/Modules/Shared/Dispo.Shared.Filter/Services/FilterService.cs
@@ -39,27 +39,27 @@ namespace Dispo.Shared.Filter.Services
                 else
                 {
                     constant = Expression.Constant(Convert.ToInt32(property.Value));
-                    switch (property.SearchType)
-                    {
-                        case SearchType.Equals:
-                            comparison = Expression.Equal(memberExpression, constant.Value);
-                            break;
-                        case SearchType.GreaterThan:
-                            comparison = Expression.GreaterThan(memberExpression, constant.Value);
-                            break;
-                        case SearchType.GreaterThanOrEqual:
-                            comparison = Expression.GreaterThanOrEqual(memberExpression, constant.Value);
-                            break;
-                        case SearchType.LessThan:
-                            comparison = Expression.LessThan(memberExpression, constant.Value);
-                            break;
-                        case SearchType.LessThanOrEqual:
-                            comparison = Expression.LessThanOrEqual(memberExpression, constant.Value);
-                            break;
-                        default:
-                            comparison = Expression.Equal(memberExpression, constant.Value);
-                            break;
-                    }
+                    //switch (property.SearchType)
+                    //{
+                    //    case SearchType.Equals:
+                    //        comparison = Expression.Equal(memberExpression, constant.Value);
+                    //        break;
+                    //    case SearchType.GreaterThan:
+                    //        comparison = Expression.GreaterThan(memberExpression, constant.Value);
+                    //        break;
+                    //    case SearchType.GreaterThanOrEqual:
+                    //        comparison = Expression.GreaterThanOrEqual(memberExpression, constant.Value);
+                    //        break;
+                    //    case SearchType.LessThan:
+                    //        comparison = Expression.LessThan(memberExpression, constant.Value);
+                    //        break;
+                    //    case SearchType.LessThanOrEqual:
+                    //        comparison = Expression.LessThanOrEqual(memberExpression, constant.Value);
+                    //        break;
+                    //    default:
+                    //        comparison = Expression.Equal(memberExpression, constant.Value);
+                    //        break;
+                    //}
                 }
 
                 filterExpression = filterExpression == null ? comparison : Expression.And(filterExpression, comparison);
