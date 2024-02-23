cab4c8834ffc8a400bab8f9406a194a5e30aeb61
Arthur Molinari <arthureduardomolinari@gmail.com>
2024-02-23 19:23:01 -0300

    104 - Filtro numérico e por data.

--- src/Modules/Shared/Dispo.Shared.Filter/Model/PropertyModel.cs
+++ src/Modules/Shared/Dispo.Shared.Filter/Model/PropertyModel.cs
@@ -3,8 +3,7 @@
     public class PropertyModel
     {
         public string Name { get; set; }
-        public string Type { get; set; }
-        public string Value { get; set; }
+        public dynamic Value { get; set; }
         public SearchType SearchType { get; set; }
     }
 
--- src/Modules/Shared/Dispo.Shared.Filter/Services/FilterService.cs
+++ src/Modules/Shared/Dispo.Shared.Filter/Services/FilterService.cs
@@ -2,12 +2,15 @@
 using Dispo.Shared.Filter.Model;
 using Dispo.Shared.Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
 using System.Linq.Expressions;
+using System.Text.Json;
 
 namespace Dispo.Shared.Filter.Services
 {
     public class FilterService : IFilterService
     {
+        private const string UnsupportedSearchTypeMessage = "Unsupported search type for property of type:";
         private readonly DispoContext _dispoContext;
 
         public FilterService(DispoContext dispoContext)
@@ -17,6 +20,11 @@ namespace Dispo.Shared.Filter.Services
 
         public List<T> Get<T>(FilterModel filterModel) where T : EntityBase
         {
+            if (filterModel is null)
+            {
+                throw new ArgumentNullException(nameof(filterModel));
+            }
+
             return _dispoContext.Set<T>()
                                 .AsNoTracking()
                                 .Where(BuildExpression<T>(filterModel))
@@ -30,38 +38,22 @@ namespace Dispo.Shared.Filter.Services
             foreach (var property in filterModel.Properties)
             {
                 var memberExpression = Expression.Property(parameter, property.Name);
-                var constant = Expression.Constant(property.Value);
                 Expression comparison;
                 if (memberExpression.Type == typeof(string))
                 {
-                    comparison = Expression.Call(memberExpression, property.SearchType.ToString(), Type.EmptyTypes, constant);
+                    comparison = GetStringComparisonExpression(memberExpression, property);
+                }
+                else if (IsNumericType(memberExpression.Type, property.Value, out ConstantExpression convertedConstant))
+                {
+                    comparison = GetGenericComparisonExpression(memberExpression, property, convertedConstant);
+                }
+                else if (IsDateTimeType(memberExpression.Type, property.Value, out DateTime convertedDateTime))
+                {
+                    comparison = GetGenericComparisonExpression(memberExpression, property, Expression.Constant(convertedDateTime));
                 }
                 else
                 {
-                    constant = Expression.Constant(Convert.ToInt32(property.Value));
-                    comparison = Expression.Equal(memberExpression, constant);
-
-                    //switch (property.SearchType)
-                    //{
-                    //    case SearchType.Equals:
-                    //        comparison = Expression.Equal(memberExpression, constant.Value);
-                    //        break;
-                    //    case SearchType.GreaterThan:
-                    //        comparison = Expression.GreaterThan(memberExpression, constant.Value);
-                    //        break;
-                    //    case SearchType.GreaterThanOrEqual:
-                    //        comparison = Expression.GreaterThanOrEqual(memberExpression, constant.Value);
-                    //        break;
-                    //    case SearchType.LessThan:
-                    //        comparison = Expression.LessThan(memberExpression, constant.Value);
-                    //        break;
-                    //    case SearchType.LessThanOrEqual:
-                    //        comparison = Expression.LessThanOrEqual(memberExpression, constant.Value);
-                    //        break;
-                    //    default:
-                    //        comparison = Expression.Equal(memberExpression, constant.Value);
-                    //        break;
-                    //}
+                    throw new NotSupportedException($"{UnsupportedSearchTypeMessage} {property.SearchType}");
                 }
 
                 filterExpression = filterExpression == null ? comparison : Expression.And(filterExpression, comparison);
@@ -69,5 +61,66 @@ namespace Dispo.Shared.Filter.Services
 
             return Expression.Lambda<Func<T, bool>>(filterExpression, parameter).Compile();
         }
+
+        private Expression GetStringComparisonExpression(MemberExpression memberExpression, PropertyModel property)
+        {
+            string convertedValue = Convert.ToString(property.Value);
+            var constant = Expression.Constant(convertedValue);
+
+            // Ao enviar o SearchType.Equals está disparando uma exceção.
+            return Expression.Call(memberExpression, property.SearchType.ToString(), Type.EmptyTypes, constant);
+        }
+
+        private Expression GetGenericComparisonExpression(MemberExpression memberExpression, PropertyModel property, ConstantExpression convertedConstant)
+        {
+            switch (property.SearchType)
+            {
+                case SearchType.Equals:
+                    return Expression.Equal(memberExpression, convertedConstant);
+                case SearchType.GreaterThan:
+                    return Expression.GreaterThan(memberExpression, convertedConstant);
+                case SearchType.GreaterThanOrEqual:
+                    return Expression.GreaterThanOrEqual(memberExpression, convertedConstant);
+                case SearchType.LessThan:
+                    return Expression.LessThan(memberExpression, convertedConstant);
+                case SearchType.LessThanOrEqual:
+                    return Expression.LessThanOrEqual(memberExpression, convertedConstant);
+                default:
+                    throw new NotSupportedException($"{UnsupportedSearchTypeMessage} {property.SearchType}");
+            }
+        }
+
+        private bool IsNumericType(Type type, object value, out ConstantExpression convertedConstant)
+        {
+            convertedConstant = null;
+            if (NumericTypes.Contains(type))
+            {
+                try
+                {
+                    var convertedValue = Convert.ChangeType(Convert.ToString(value), type);
+                    convertedConstant = Expression.Constant(convertedValue);
+                    return true;
+                }
+                catch (InvalidCastException)
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsDateTimeType(Type type, object value, out DateTime convertedDateTime)
+        {
+            convertedDateTime = DateTime.MinValue;
+            return type == typeof(DateTime) && DateTime.TryParse(Convert.ToString(value), out convertedDateTime);
+        }
+
+        private static readonly HashSet<Type> NumericTypes = new HashSet<Type>
+        {
+            typeof(int), typeof(double), typeof(float), typeof(decimal),
+            typeof(long), typeof(short), typeof(byte), typeof(uint),
+            typeof(ulong), typeof(ushort), typeof(sbyte)
+        };
     }
 }
